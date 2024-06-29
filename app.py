da1daff7c8f81a95b6a1eeeb7274591d8fe579f3
hongphong177 <hong.phong@diqit.io>
2024-06-29 20:52:28 +0700

    update code
    update code

--- app.py
+++ app.py
@@ -3,23 +3,30 @@ from PIL import Image
 from openai import OpenAI
 import io
 import base64
+import json
+
 
 api_key = ''
 client = OpenAI(api_key=api_key)
 
 
-def classify_labels_with_gpt4(description, fileBase64):
+def classify_labels_with_gpt4(fileBase64):
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=[
             {"role": "system",
-                "content": "You are an assistant that classifies image labels."},
+                "content": """You are an specialist beer event assistant that classifies image labels about these brands with logo and product url:
+                Heineken product: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU0hCIdc50nuj-Kj7-qD-EfHebm5NAzVCZoQ&s,
+                learn carefully Promotion material images: https://drive.google.com/drive/folders/11WmGXKAtEVxDMfwpvdJ_NUlhPiqXB1yA and list all materials has Heineken logo in the image
+                call save_analysis function to save the analysis every time  detect image.
+                If don't have any heineken logo in the image, please return nothing.
+                """},
             {
                 "role": "user",
                 "content": [
                     {
                         "type": "text",
-                        "text": f"Classify the following description into number of customers, promotion girls, and type of place: {description}"
+                        "text": f"Classify the following description into what brand ,number of customers, promotion girls, and type of place"
                     },
                     {
                         "type": "image_url",
@@ -31,29 +38,72 @@ def classify_labels_with_gpt4(description, fileBase64):
             }
         ],
         max_tokens=150,
+        temperature=0.1,
         tools=[
             {
                 "type": "function",
                 "function": {
-                    "name": "get_weather",
-                    "description": "Determine weather in my location",
+                    "name": "save_analysis",
+                    "description": "Save the analysis to a database",
                     "parameters": {
                         "type": "object",
                         "properties": {
-                            "location": {
+                            "decorator_items": {
                                 "type": "string",
-                                "description": "The city and state e.g. San Francisco, CA"
+                                "description": "List of Heineken decoration items in the image (banner, standee, beer crate)"
                             },
-                            "unit": {
+                            "heineken_drinker": {
+                                "type": "number",
+                                "description": "number of Heineken beer drinker in the image"
+                            },
+                            "promotion_girl": {
+                                "type": "number",
+                                "description": "number of customer in the image exclude promotion girls"
+                            },
+                            "activities": {
                                 "type": "string",
+                                "description": "what activities are in the image: drinking, talking, playing game, Eating, Smiling, Shopping, etc."
+                            },
+                            "emotions": {
+                                "type": "string",
+                                "description": "what emotions are in the image: happy, sad, angry, enjoyable, neutral etc."
+                            },
+                            "heineken_products": {
+                                "type": "string",
+                                "description": "what products have heineken logo",
+                            },
+                            "promotion_materials": {
+                                "type": "string",
+                                "description": "what promotion materials are in the image: POSM, banner, poster, etc."
+                            },
+                            "brand_logos": {
+                                "type": "string",
+                                "description": "Is heineken logo in the image",
                                 "enum": [
-                                    "c",
-                                    "f"
+                                    "true",
+                                    "false"
                                 ]
+                            },
+                            "scenes": {
+                                "type": "string",
+                                "description": "what scenes are in the image: bar, pub, restaurant, etc."
+                            },
+                            "environments": {
+                                "type": "string",
+                                "description": "what environment are in the image: indoor, outdoor, etc."
                             }
                         },
                         "required": [
-                            "location"
+                            "decorator_items",
+                            "heineken_drinker",
+                            "promotion_girl",
+                            "activities",
+                            "emotions",
+                            "heineken_products",
+                            "promotion_materials",
+                            "brand_logos",
+                            "scenes",
+                            "environments",
                         ]
                     }
                 }
@@ -61,7 +111,61 @@ def classify_labels_with_gpt4(description, fileBase64):
         ]
     )
 
+    tool_call = response.choices[0].message.tool_calls[0]
+    if (tool_call.function.name == "save_analysis"):
+        args_json = tool_call.function.arguments
+        args = json.loads(args_json)
+        save_analysis(
+            args['decorator_items'],
+            args['heineken_drinker'],
+            args['promotion_girl'],
+            args['activities'],
+            args['emotions'],
+            args['heineken_products'],
+            args['promotion_materials'],
+            args['brand_logos'],
+            args['scenes'],
+            args['environments'],
+        )
+
     return response.choices[0].message.content
+
+
+def save_analysis(decorator_items="", heineken_drinker=0, promotion_girl=0, activities="", emotions="", heineken_products="", promotion_materials="", brand_logos="", scenes="", environments=""):
+    point = 0
+    if (decorator_items != ""):
+        st.write("decorator_items:", decorator_items)
+
+    if (heineken_drinker != 0):
+        point += 1
+        st.write("heineken_drinker:", heineken_drinker)
+
+    if (promotion_girl != 0):
+        if (promotion_girl >= 2):
+            point += 1
+
+        st.write("promotion girl:", promotion_girl)
+
+    if (activities != ""):
+        st.write("activities:", activities)
+
+    if (emotions != ""):
+        st.write("emotions:", emotions)
+
+    if (heineken_products != ""):
+        st.write("heineken_products:", heineken_products)
+
+    if (promotion_materials != ""):
+        st.write("promotion_materials:", promotion_materials)
+
+    if (brand_logos != ""):
+        st.write("brand_logos:", brand_logos)
+
+    if (scenes != ""):
+        st.write("scenes:", scenes)
+
+    if (environments != ""):
+        st.write("environments:", environments)
 
 
 def image_to_base64(image):
@@ -93,15 +197,12 @@ uploaded_files = st.file_uploader(
 if uploaded_files:
     for uploaded_file in uploaded_files:
         image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+        st.image(image, caption='Uploaded Image',
+                 width=150, use_column_width=False)
         st.write("")
-
-        description = st.text_area("Describe the image")
 
         if st.button("Analyze"):
             # resized_img = image.resize((150, 100))
             # st.write("Description:", compress_image(image, new_height=150))
             st.write("Analyzing...")
-            classification = classify_labels_with_gpt4(
-                description, compress_image(image, new_height=150))
-            st.write("Classified Labels:", classification)
+            classify_labels_with_gpt4(compress_image(image, new_height=150))
